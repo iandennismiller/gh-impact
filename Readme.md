b384bee50ba8555ca632566c4bc5d9e16bfe9cbf
Ian Dennis Miller <ian@iandennismiller.com>
2016-08-02 21:32:05 -0400

    todo items

--- Readme.md
+++ Readme.md
@@ -2,17 +2,17 @@
 
 ## Todo
 
-- [ ] hit enter to search
-- [ ] put the query in the location history
-- [ ] when loaded with location hash, perform search automatically
+- [x] hit enter to search
+- [x] put the query in the location history
+- [x] when loaded with location hash, perform search automatically
 - [ ] get a domain, probably
 - [ ] write about page
 - [ ] choose a few interesting defaults and make them clickable
-- [ ] focus input in text box
+- [x] focus input in text box
 - [ ] able to tab to search button?
 - [ ] google analytics
 - [ ] density plot of scores to answer question: how do I compare?
 - [ ] lowercase all names so case is insensitive
-- [ ] error response if name not found
+- [x] error response if name not found
 - [ ] use github API to retrieve other details about the user
-- [ ] bigger search box
+- [x] bigger search box
--- gh-impact.js
+++ gh-impact.js
@@ -3,16 +3,39 @@ Ian Dennis Miller
 */
 
 var query = function() {
-  $("#masked").css("display", "block");
-  var account_name = $("#account_name_query").val();
-  var first_letter = account_name[0];
-  $.getJSON( "data/" + first_letter + ".json", function(data) {
-    $("#account_name").html("<a target='_blank' href='http://github.com/" + account_name + "'>" + account_name + "</a>");
-    $("#impact_score").html(data[account_name]);
-  });
+    $("#masked").css("display", "block");
+    var account_name = $("#account_name_query").val();
+    var first_letter = account_name[0];
+    $.getJSON( "data/" + first_letter + ".json", function(data) {
+        if (data[account_name] != undefined) {
+            $("#account_name").html("<a target='_blank' href='http://github.com/" + account_name + "'>" + account_name + "</a>");
+            $("#impact_score").html(data[account_name]);
+            location.hash = account_name;
+        }
+        else {
+            $("#account_name").html("not found");
+            $("#impact_score").html("");
+        }
+    });
 }
 
 $(function() {
-  $("#search").removeAttr("href");
-  $("#search").click(query);
+    $("#search").removeAttr("href");
+
+    $("#search").click(query);
+
+    $('#account_name_query').keypress(function (e) {
+        if (e.which == 13) {
+            // $('form#login').submit();
+            query();
+            return false;
+        }
+    });
+
+    if (location.hash != "") {
+        var q = location.hash;
+        q = q.substring(1, location.hash.length);
+        $("#account_name_query").val(q);
+        query();
+    }
 });
